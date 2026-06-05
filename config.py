75e4d7fcfac9ed31ba6800c550de07ac822a1516
alexbwolf <83029361+alexbwolf@users.noreply.github.com>
2026-06-05 15:52:11 +0200

    Small change with big impact!

--- config.py
+++ config.py
@@ -238,7 +238,12 @@ class EnvConfig:
         # List of mappings for params for distillation, split, add_solvent
         self.DF_distillation_map = np.linspace(0.01, 0.99, 100)
         self.split_ratio_map = np.linspace(0.01, 0.99, 100)
-        self.add_solvent_comp_map = np.linspace(0.01, 9.99, 100)
+        _add_solvent_amount_grid = np.linspace(0.01, 9.99, 100)
+
+        self.add_solvent_comp_map = {
+            name: _add_solvent_amount_grid.copy()
+            for name in self.component_names
+        }
 
 
     # Random feed generator
